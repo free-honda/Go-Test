3a6d7b929e2ebe92bcef91b253e647c6ef790484
free-honda <154935293+free-honda@users.noreply.github.com>
2025-01-22 22:30:02 +0900

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	// "Hello, World!" を返すハンドラー
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
+		return c.String(http.StatusOK, "Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 	})
 
 	// サーバーを起動 (外部アクセス用に 0.0.0.0 にバインド)
